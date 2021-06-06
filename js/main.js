ab3169d300d2b17d288a2d13df835870d7d1b5d0
leducgiachoang <69897727+leducgiachoang@users.noreply.github.com>
2021-06-06 17:24:01 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,109 +1,24 @@
-
-        function mySubmitFunction(e) {
-            e.preventDefault();
-            return false;
-        }
-
-
-
-        jQuery(document).ready(function () {
-            jQuery('#button_search').click(function () {
-                var value_key = jQuery('#text_search').val();
-                jQuery('#button_next').css({
-                    'display': 'block',
-                })
-                const container = document.querySelector(".container");
-                container.innerHTML = '';
-
-                function getPhotos(images) {
-                    images.map(image => {
-                        var cardTag = `<div class="card_img">
+function mySubmitFunction(e){e.preventDefault();return!1}
+jQuery(document).ready(function(){jQuery('#button_search').click(function(){var value_key=jQuery('#text_search').val();jQuery('#button_next').css({'display':'block',});const container=document.querySelector(".container");container.innerHTML='';function getPhotos(images){images.map(image=>{var cardTag=`<div class="card_img">
                             <a class="elem" href="${image.src.landscape}" data-lcl-author="${image.photographer}" data-lcl-thumb="${image.src.landscape}">
                                     <span style="background-image: url(${image.src.tiny});"></span>
                             </a>
-                        </div>`;
-                        container.innerHTML += cardTag;
-                    })
-                }
-
-                fetch("https://api.pexels.com/v1/search/?page=1&per_page=80&query=" + value_key, {
-                        headers: {
-                            Authorization: "563492ad6f917000010000011da97604116d4926aa3bd66d8e472c57"
-                        }
-                    })
-                    .then(resp => {
-                        return resp.json()
-                    })
-                    .then(data => {
-                        getPhotos(data.photos);
-                        var link_next = data.next_page;
-                        document.getElementById('button_next').setAttribute('link-next',
-                            link_next)
-                        console.log(data)
-                    })
-
-                $('#button_next').click(function () {
-                    var link_next = document.getElementById('button_next').getAttribute(
-                        'link-next');
-
-                    if (link_next == 'undefined') {
-                        jQuery('#button_next').css({
-                            'display': 'none',
-                        })
-                    }
-
-                    fetch(link_next, {
-                            headers: {
-                                Authorization: "563492ad6f917000010000011da97604116d4926aa3bd66d8e472c57"
-                            }
-                        })
-                        .then(resp => {
-                            return resp.json()
-                        })
-                        .then(data => {
-                            getPhotos(data.photos);
-                            var link_next = data.next_page;
-                            document.getElementById('button_next').setAttribute('link-next',
-                                link_next)
-                        })
-                })
-            })
-
-
-        })
-
-        function randomimg() {
-            var min = 1
-            var max = 5
-            var number = Math.floor(Math.random() * (max - min + 1)) + min;
-
-            switch (number) {
-                case 1:
-                    document.getElementById('image').setAttribute('src',
-                        'imgs/hinh1.jpg')
-                    break;
-
-                case 2:
-                    document.getElementById('image').setAttribute('src',
-                        'imgs/hinh2.jpg')
-                    break;
-
-                case 3:
-                    document.getElementById('image').setAttribute('src',
-                        'imgs/hinh3.jpg')
-                    break;
-
-                case 4:
-                    document.getElementById('image').setAttribute('src',
-                        'imgs/hinh4.jpg'
-                    )
-                    break;
-
-                case 5:
-                    document.getElementById('image').setAttribute('src',
-                        'imgs/hinh5.jpg'
-                    )
-                    break;
-            }
-        }
-        setInterval(randomimg, 3000);
+                        </div>`;container.innerHTML+=cardTag})}
+fetch("https://api.pexels.com/v1/search/?page=1&per_page=80&query="+value_key,{headers:{Authorization:"563492ad6f917000010000011da97604116d4926aa3bd66d8e472c57"}}).then(resp=>{return resp.json()}).then(data=>{getPhotos(data.photos);var link_next=data.next_page;document.getElementById('button_next').setAttribute('link-next',link_next)})});$('#button_next').click(function(){var link_next=document.getElementById('button_next').getAttribute('link-next');if(link_next=='undefined'){jQuery('#button_next').css({'display':'none',})}
+fetch(link_next,{headers:{Authorization:"563492ad6f917000010000011da97604116d4926aa3bd66d8e472c57"}}).then(resp=>{return resp.json()}).then(data=>{getPhotos(data.photos);var link_next=data.next_page;document.getElementById('button_next').setAttribute('link-next',link_next)})})
+const container=document.querySelector(".container");container.innerHTML='';function getPhotos(images){images.map(image=>{var cardTag=`<div class="card_img">
+                            <a class="elem" href="${image.src.landscape}" data-lcl-author="${image.photographer}" data-lcl-thumb="${image.src.landscape}">
+                                    <span style="background-image: url(${image.src.tiny});"></span>
+                            </a>
+                        </div>`;container.innerHTML+=cardTag})}
+fetch("https://api.pexels.com/v1/search/?page=1&per_page=80&query=vietnam",{headers:{Authorization:"563492ad6f917000010000011da97604116d4926aa3bd66d8e472c57"}}).then(resp=>{return resp.json()}).then(data=>{getPhotos(data.photos);var link_next=data.next_page;document.getElementById('button_next').setAttribute('link-next',link_next)})})
+function randomimg(){var min=1
+var max=5
+var number=Math.floor(Math.random()*(max-min+1))+min;switch(number){case 1:document.getElementById('image').setAttribute('src','imgs/hinh1.jpg')
+break;case 2:document.getElementById('image').setAttribute('src','imgs/hinh2.jpg')
+break;case 3:document.getElementById('image').setAttribute('src','imgs/hinh3.jpg')
+break;case 4:document.getElementById('image').setAttribute('src','imgs/hinh4.jpg')
+break;case 5:document.getElementById('image').setAttribute('src','imgs/hinh5.jpg')
+break}}
+setInterval(randomimg,3000);$(window).bind("load",function(){jQuery("#status").fadeOut();jQuery("#loader").fadeOut()});$(document).ready(function(e){lc_lightbox('.elem',{wrap_class:'lcl_fade_oc',gallery:!0,thumb_attr:'data-lcl-thumb',skin:'minimal',radius:0,padding:0,border_w:0,})});var mybutton=document.getElementById("myBtn");window.onscroll=function(){scrollFunction()};function scrollFunction(){if(document.body.scrollTop>20||document.documentElement.scrollTop>20){mybutton.style.display="block"}else{mybutton.style.display="none"}}
+function topFunction(){document.body.scrollTop=0;document.documentElement.scrollTop=0}
